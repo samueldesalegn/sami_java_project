ea93a9a14857e0e2bbccf569b512312c4cf3bdff
Samuel <stumdedo@miu.edu>
2022-02-24 18:03:10 -0600

    initial commit

---
+++ assignment3/changeMaker.java
@@ -0,0 +1,43 @@
+package assignment3;
+
+import java.util.Scanner;
+
+public class changeMaker {
+    public static void main(String[] args) {
+
+        int amount;
+        int quarters = 0;
+        int dimes = 0;
+        int nickles = 0;
+        int pennies = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the amount you want to change");
+        amount = input.nextInt();
+
+        int originalAmount = amount;
+        
+        // 95
+        quarters = amount/25; 
+        amount = amount % 25;
+        dimes = amount / 10;
+        amount = amount % 10;
+        nickles = amount / 5;
+        amount = amount % 5;
+        pennies = amount;
+
+
+        System.out.println("The amount of " + originalAmount + " can be broken into: ");
+        System.out.println("Quarters: " + quarters);
+        System.out.println("Dimes: " + dimes);
+        System.out.println("Nickles: " + nickles);
+        System.out.println("Pennies: " + pennies);
+
+
+
+
+        
+    }
+}
+
---
+++ assignment3/firstProgram.java
@@ -0,0 +1,22 @@
+package assignment3;
+
+import java.util.Scanner;
+
+public class firstProgram {
+    public static void main(String[] args) {
+
+        System.out.println("Hello out there.");
+        System.out.println("I will add three numbers for you.");
+        System.out.println("Enter three whole numbers on a line:");
+
+        int n1, n2, n3;
+
+        Scanner keyBoard = new Scanner(System.in);
+        n1 = keyBoard.nextInt();
+        n2 = keyBoard.nextInt();
+        n3 = keyBoard.nextInt();
+
+        System.out.println("The sum of those three numbers is" );
+        System.out.println(n1 + n2 + n3);    
+    }
+}
