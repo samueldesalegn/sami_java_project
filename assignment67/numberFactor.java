f5f952115cd5ead05beae7115bbeb9b290a6c889
Samuel Desalegn Tumdedo <64654098+samueldesalegn@users.noreply.github.com>
2022-03-01 20:15:57 -0600

    indentation again

--- assignment67/numberFactor.java
+++ assignment67/numberFactor.java
@@ -3,22 +3,22 @@ package assignment67;
 import java.util.Scanner;
 
 public class numberFactor {  
-	public static void main(String[] args) {
-		int number, factor;
+  public static void main(String[] args) {
+  int number, factor;
 	
-    try (Scanner keyBoard = new Scanner(System.in)) {
-		  System.out.println("Enter a number");
-	    number = keyBoard.nextInt();
-			System.out.println("Enter a factor number");
-	    factor = keyBoard.nextInt();
+  try (Scanner keyBoard = new Scanner(System.in)) {
+    System.out.println("Enter a number");
+    number = keyBoard.nextInt();
+    System.out.println("Enter a factor number");
+    factor = keyBoard.nextInt();
 		
 
-		  if(number % factor == 0) {
-		    System.out.println(number +" is divisible by " + factor +"!");
+    if(number % factor == 0) {
+      System.out.println(number +" is divisible by " + factor +"!");
 
-			} else {
-				System.out.println(number +" is not divisible by " + factor +"!");
-		  }
-		}	
+    }else {
+       System.out.println(number +" is not divisible by " + factor +"!");
+    }
+   }	
   }    
 }
