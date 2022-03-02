f283c102b5e596fcc9d27a3d849ee24c20bc9d08
Samuel Desalegn Tumdedo <64654098+samueldesalegn@users.noreply.github.com>
2022-03-01 20:12:12 -0600

    indenting

--- assignment67/validTestScore.java
+++ assignment67/validTestScore.java
@@ -3,20 +3,20 @@ package assignment67;
 import java.util.Scanner;
 
 public class validTestScore {  
-	public static void main(String[] args) {
-		int score;
+  public static void main(String[] args) {
+    int score;
 	
     try (Scanner keyBoard = new Scanner(System.in)) {
-		  System.out.println("Enter a number");
-	    score = keyBoard.nextInt();
+      System.out.println("Enter a number");
+      score = keyBoard.nextInt();
 
-		  if(score >= 0 && score <= 100) {
-		    System.out.println("Valid Test Score!");
+      if(score >= 0 && score <= 100) {
+        System.out.println("Valid Test Score!");
 
-			} else {
-				System.out.println("Invalid Test Score!");
+      }else {
+	System.out.println("Invalid Test Score!");
 				
-			}
-	  }
+      }
+   }
   }    
 }
