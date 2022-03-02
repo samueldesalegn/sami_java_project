034144beef4db0ffc623f8e9e6ff8d58ed3ad1a1
Samuel Desalegn Tumdedo <64654098+samueldesalegn@users.noreply.github.com>
2022-03-02 08:22:44 -0600

    Switch case code modified

--- assignment67/gradeWithSwitch.java
+++ assignment67/gradeWithSwitch.java
@@ -1,36 +1,40 @@
 package assignment67;
 
+package assignment67;
+
 import java.util.Scanner;
 
 public class gradeWithSwitch {  
-	public static void main(String[] args) {
+  public static void main(String[] args, Object Sysetm) {
 
-		char grade = 'B';
+    char letter = 0;
+    double gradeValue;
+
+    try (Scanner keyBoard = new Scanner(System.in)) {
+      System.out.println("Enter  your gradeValue: ");
+      letter = keyBoard.next().charAt(0);
+    }  			
+    switch (letter) {
+      case 'A':
+        gradeValue = 4.0;
+	break;
+      case 'B':
+	gradeValue = 3.0;
+	break;
+      case 'C':
+	gradeValue = 2.0;
+	break;
+      case 'D':
+	gradeValue = 1.0;
+	break;
+      case 'F':
+	gradeValue = 0.0;
+
+      default:
+	gradeValue = 0.0;
+	System.out.println("Try again letter");		
 		
-
-		// try (Scanner keyBoard = new Scanner(System.in)) {
-		//   System.out.println("Enter  your grade: ");
-	  //   grade = keyBoard.next();
-			
-		switch (grade) {
-			case 'A':
-				System.out.println("Your grade value is 4.0" );
-				break;
-			case 'B':
-				System.out.println("Your grade value is 3.0");
-				break;
-			case 'C':
-				System.out.println("Your grade value is 2.0");
-				break;
-			case 'D':
-				System.out.println("Your grade value is 1.0");
-				break;
-			case 'F':
-				System.out.println("Your grade value is 0.0");
-
-			default:
-				System.out.println("Try Again with another input");		
-			
-    }    
+      }  
+      System.out.println("The grade value is " + gradeValue); 
   }
 }
