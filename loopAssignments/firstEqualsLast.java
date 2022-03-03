e051ca0cdcebc74a8a83db4e5597f81449d77a33
Samuel Desalegn Tumdedo <64654098+samueldesalegn@users.noreply.github.com>
2022-03-03 16:53:00 -0600

    The first letter is equal to last letter

--- loopAssignments/firstEqualsLast.java
+++ loopAssignments/firstEqualsLast.java
@@ -2,27 +2,27 @@ package loopAssignments;
 
 import java.util.Scanner;
 
-public class firstEqualsLast {  
-	public static void main(String[] args) {
+public class firstEqualsLast {
+  public static void main(String[] args) {
 		
-		try (Scanner keyBoard = new Scanner(System.in)) {
-			String word = "";
+  try (Scanner keyBoard = new Scanner(System.in)) {
+    String word = "";
 
-			while(!word.equals("done")) { 
-		  System.out.println("\nEnter  words.");
-	    word = keyBoard.next();
+    while(!word.equals("done")) { 
+      System.out.println("\nEnter  words.");
+      word = keyBoard.next();
 
-			if(word.equals("done")) {
-				break;
-			}
+      if(word.equals("done")) {
+	break;
+      }
 
-			if(word.charAt(0) == word.charAt(word.length()-1)) {
-				System.out.println("The first letter of the word is equal to the last words.");
-			} else {System.out.println("The first letter of the word is not equal to the last words.");}
+      if(word.charAt(0) == word.charAt(word.length()-1)) {
+	System.out.println("The first letter of the word is equal to the last words.");
+      } else {System.out.println("The first letter of the word is not equal to the last words.");}
 
-			}
+      }
 
-	  }
+    }
   }    
 }
 
