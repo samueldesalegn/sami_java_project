988def7af15192e95ba2950d0a457b6f1b683225
Samuel Desalegn Tumdedo <64654098+samueldesalegn@users.noreply.github.com>
2022-03-03 16:59:16 -0600

    Sum of N odd numbers

--- loopAssignments/sumOddNumbers.java
+++ loopAssignments/sumOddNumbers.java
@@ -3,20 +3,19 @@ package loopAssignments;
 import java.util.Scanner;
 
 public class sumOddNumbers {  
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 		
-		int n;
-		try (Scanner keyBoard = new Scanner(System.in)) {
-		  System.out.println("\nEnter a number ");
-	    n = keyBoard.nextInt();
-			
-			int i =0, sum =0;
-			while(i < n) { 
-				sum += (i*2+1);
-				i++;
-			}
-			System.out.println(sum);
-
-	  }
+  int n;
+  try (Scanner keyBoard = new Scanner(System.in)) {
+    System.out.println("\nEnter a number ");
+    n = keyBoard.nextInt();
+  }		
+  int i =0, sum =0;
+  while(i < n) { 
+    sum += (i*2+1);
+    i++;
+  }
+  System.out.println(sum);
+   
   }    
 }
