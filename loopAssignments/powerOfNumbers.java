c22a690d066976ade723e260f4f6c2f3dd4cbd8f
Samuel Desalegn Tumdedo <64654098+samueldesalegn@users.noreply.github.com>
2022-03-03 16:55:27 -0600

    sum of squared numbers

--- loopAssignments/powerOfNumbers.java
+++ loopAssignments/powerOfNumbers.java
@@ -3,19 +3,19 @@ package loopAssignments;
 import java.util.Scanner;
 
 public class powerOfNumbers {  
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 		
-		int n;
-		try (Scanner keyBoard = new Scanner(System.in)) {
+  int n;
+  try (Scanner keyBoard = new Scanner(System.in)) {
+    System.out.println("\nEnter a number.");
+    n = keyBoard.nextInt();
+  }	
 
-			System.out.println("\nEnter a number.");
-	    n = keyBoard.nextInt();
-		}	
-			int sum = 0;
-			for (int i = 1; i <= n; i++) {
-				sum += Math.pow(i, 2);
-			}
-		  System.out.println(sum);
+  int sum = 0;
+  for (int i = 1; i <= n; i++) {
+    sum += Math.pow(i, 2);
+  }
+  System.out.println(sum);
 		
   }    
 }
