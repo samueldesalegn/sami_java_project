60148fd5a0846151be1cf33c69d47c5ded38a057
Samuel Desalegn Tumdedo <64654098+samueldesalegn@users.noreply.github.com>
2022-03-07 21:44:57 -0600

    modified indentation

--- assignment10/CoinHeadTailCount.java
+++ assignment10/CoinHeadTailCount.java
@@ -2,34 +2,34 @@ package assignment10;
 import java.util.Scanner;
 
 public class CoinHeadTailCount {
-	public static void main(String[] args) {
-		String coinToss; 
+  public static void main(String[] args) {
+  String coinToss; 
 
      
-		try (Scanner keyboard = new Scanner(System.in)) { 
-			System.out.println("For each coin toss enter either 'h' for heads or 't' for tails.");
+  try (Scanner keyboard = new Scanner(System.in)) { 
+    System.out.println("For each coin toss enter either 'h' for heads or 't' for tails.");
 
-   	  int headCount =0;
-		  int tailCount = 0;
-
-			for(int i = 0; i < 8; i++) {
-				System.out.println("Enter the next toss"); //coin toss is either tail or head.
-				coinToss = keyboard.nextLine();
-
-				if(coinToss.equalsIgnoreCase("h")) {
-					headCount++;
-				}else if(coinToss.equalsIgnoreCase("t")){
-					tailCount++;
-				}
-			}
-			System.out.println("The total head count: " + headCount);
-			System.out.println("The total tail count: " + tailCount);
+    int headCount =0;
+    int tailCount = 0;
+ 
+    for(int i = 0; i < 8; i++) {
+      System.out.println("Enter the next toss"); //coin toss is either tail or head.
+      coinToss = keyboard.nextLine();
+  }
+      if(coinToss.equalsIgnoreCase("h")) {
+	headCount++;
+      }else if(coinToss.equalsIgnoreCase("t")){
+	tailCount++;
+      }
+    }
+    System.out.println("The total head count: " + headCount);
+    System.out.println("The total tail count: " + tailCount);
       
-			double headPercent = (double)headCount* 100/8;
-			double tailPercent = (double)tailCount * 100/8;
+    double headPercent = (double)headCount* 100/8;
+    double tailPercent = (double)tailCount * 100/8;
 				
-			System.out.println("Percent of heads: " + headPercent);  
-			System.out.println("Percent of tails: "+ tailPercent); 
-		}
-	}
+    System.out.println("Percent of heads: " + headPercent);  
+    System.out.println("Percent of tails: "+ tailPercent); 
+  }
 }
+	
