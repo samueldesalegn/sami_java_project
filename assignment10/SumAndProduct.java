9841672b81f7445c6076d73c796b9ee626665207
Samuel Desalegn Tumdedo <64654098+samueldesalegn@users.noreply.github.com>
2022-03-07 22:21:22 -0600

    added the question/problem part

--- assignment10/SumAndProduct.java
+++ assignment10/SumAndProduct.java
@@ -1,3 +1,28 @@
+// 2. What does the following fragment of code display? What do you think the 
+// programmer intended the code to do, and how would you fix it
+//   int max = 20;
+//   int sum = 0;
+//   intproduct = 1;
+  
+//    for(int i = 1; i <= max; i++) 
+
+//     sum += i;
+//     product *= i;
+  
+//   System.out.println("The sum is " + sum + " and the product is " + product);  
+
+// Solution:
+
+//The programmer looks to seek to solve the product and sum of iterations with max and min values. 
+// For example, taking the maximum number 20:
+// sum = 0 + 1 + 2 + 3 + 4 + 5 + ...... + 20;
+// product = 1*2*3*4*5*6......*20;
+
+// However, he/she missed forming proper blocks (curly braces for the loop) and product range is beyond integer.
+
+//My Solution:
+
+
 package assignment10;
 import java.util.Scanner;
 
