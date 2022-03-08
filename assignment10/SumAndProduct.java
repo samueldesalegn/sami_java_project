a0d1778ec6fb9e5ccb0d29f9b082fa3d7d8f8329
Samuel Desalegn Tumdedo <64654098+samueldesalegn@users.noreply.github.com>
2022-03-07 21:52:39 -0600

    checking errors

--- assignment10/SumAndProduct.java
+++ assignment10/SumAndProduct.java
@@ -2,21 +2,21 @@ package assignment10;
 import java.util.Scanner;
 
 public class SumAndProduct {
-	public static void main(String[] args) {
-		int max = 20;
-		int sum = 0;
-		double product = 1;
+  public static void main(String[] args) {
+  int max = 20;
+  int sum = 0;
+  double product = 1;
 
      
-		try (Scanner keyboard = new Scanner(System.in)) { 
-			System.out.println("Enter a number \"max\" ");
-			max = keyboard.nextInt();
-		}
-			for(int i = 1; i <= max; i++) {
+  try (Scanner keyboard = new Scanner(System.in)) { 
+    System.out.println("Enter a number \"max\" ");
+    max = keyboard.nextInt();
+  }
+  for(int i = 1; i <= max; i++) {
 
-				sum += i;
-				product *= i;
-			}		
-			System.out.println("The sum is " + sum + " and the product is " + product);  
-	}
+    sum += i;
+    product *= i;
+  }		
+  System.out.println("The sum is " + sum + " and the product is " + product);  
+  }
 }
